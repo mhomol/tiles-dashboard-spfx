792edc54ec6e4b9079a17bbaa57934004315493f
Mike Homol <mhomol@threewill.com>
2020-06-25 21:46:53 -0400

    standard spfx web part initialized; no new code yet; .devcontainer work initialized

---
+++ src/webparts/tilesDashboard/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITilesDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TilesDashboardWebPartStrings' {
+  const strings: ITilesDashboardWebPartStrings;
+  export = strings;
+}
